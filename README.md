77a79e4f87246eccc02def854fb140edb1b518a0
Jared Mellentine <jared@mellentine.com>
2017-08-10 05:59:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. `cp .env.template .env`
+2. Enter GitHub username and password in .env. Update pack-o-bot dist path, if necessary.
+3. After building pack-o-bot, run `php release.php {version} {description}`
---
+++ release.php
@@ -0,0 +1,44 @@
+<?php
+
+require 'vendor/autoload.php';
+
+(new Dotenv\Dotenv(__DIR__))->load();
+
+if ($argc !== 3) {
+	die('php release.php {version} {description}' . PHP_EOL);
+}
+
+list($file, $version, $description) = $argv;
+
+$owner = 'mlntn';
+$repo  = 'pack-o-bot-test';
+
+$tag_name   = $version;
+$target     = 'master';
+$name       = "pack-o-bot {$version}";
+$body       = $description;
+$draft      = false;
+$prerelease = true;
+
+$client = new GitHubClient;
+$client->setCredentials(getenv('GITHUB_USERNAME'), getenv('GITHUB_PASSWORD'));
+
+echo 'Creating release... ';
+
+$release = $client->repos->releases->create($owner, $repo, $tag_name, $target, $name, $body, $draft, $prerelease);
+
+echo 'done.' . PHP_EOL;
+
+foreach ([ 'mac', 'win' ] as $platform) {
+	$filePath    = getenv('PACKOBOT_DIST_PATH') . "pack-o-bot-{$platform}.zip";
+	$contentType = 'application/zip';
+	$name        = "pack-o-bot-{$version}-{$platform}.zip";
+
+	echo "Adding asset '$name'... ";
+
+	$client->repos->releases->assets->upload($owner, $repo, $release->getId(), $name, $contentType, $filePath);
+
+	echo 'done.' . PHP_EOL;
+}
+
+echo 'Successfully created release.' . PHP_EOL;
